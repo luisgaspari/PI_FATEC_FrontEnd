09c731d8dda1bab945106d5cf96dee92cfbfc843
Caio Assis <caio.ab.assis@gmail.com>
2023-05-31 19:05:37 -0300

    Adiciona clienteList.js

---
+++ js/clienteList.js
@@ -0,0 +1,29 @@
+function atualizarClientes() {
+
+    const ul = document.getElementById('listaClientes')
+    ul.innerHTML = ''
+
+    const clientes = fetch('http://localhost:3000/clientes')
+    .then(resposta => resposta.json())
+    .then(clientesDados => {
+        clientesDados.forEach(cliente => {
+            const par = document.createElement('p')
+            const li = document.createElement('li')
+            li.className = "list"
+            li.innerText = `ID: ${cliente.id} - Nome: ${cliente.nome}
+            E-mail: ${cliente.email}
+            Telefone: ${cliente.telefone}`
+            /*//botao Deletar
+            const btnDelete = document.createElement('button')
+            btnDelete.innerText = "Remover"
+            btnDelete.addEventListener('click', () => deletarCliente(cliente.id))
+            li.appendChild(btnDelete)
+            //*/
+            const ul = document.getElementById('listaClientes')
+            ul.appendChild(li)
+            ul.appendChild(par)
+        }) 
+    })
+}
+
+atualizarClientes()
