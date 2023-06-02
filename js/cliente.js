8c7652545bbc9d515b48c4d53446f6abec5c1c1b
Caio Assis <caio.ab.assis@gmail.com>
2023-06-02 02:42:41 -0300

    Adiciona Create e Update em Cliente

--- js/cliente.js
+++ js/cliente.js
@@ -1,38 +1,3 @@
-
-
-const formCadastro = document.getElementById('formCadastro')
-formCadastro.addEventListener('submit', (event) => {
-    event.preventDefault()
-    cadastrarContato(event)
-})
-
-function modificarCliente(id){
-    const clienteAtt = {
-        nome: form.target.nome.value,
-        cpf: form.target.cpf.value,
-        telefone: form.target.telefone.value,
-        email: form.target.email.value,
-        cep: form.target.cep.value,
-        estado: form.target.estado.value,
-        cidade: form.target.cidade.value,
-        bairro: form.target.bairro.value,
-        endereco: form.target.endereco.value,
-        complemento: form.target.complemento.value
-    }
-    fetch(`http://localhost:3000/clientes/${id}`,{
-        method: 'PUT',
-        headers: {'Content-type':'application/json'},
-        body: JSON.stringify(clienteAtt)
-    })
-    .then(resposta => {
-        if(resposta.status != 200){
-            alert('Erro ao modificar!')
-        }
-        else alert('Modificado com sucesso!')
-        window.location.href = "cliente.html"
-    })
-}
-
 function criarCliente(form){
     const clienteNovo = {
         nome: form.target.nome.value,
@@ -52,15 +17,17 @@ function criarCliente(form){
         body: JSON.stringify(clienteNovo)
     })
     .then(resposta => {
-        if(resposta.status != 200){
+        if(resposta.status != 201){
             alert('Erro ao cadastrar!')
         }
-        else alert('Cadastrado com sucesso!')
-        atualizarContatos()
+        else {
+            alert('Cadastrado com sucesso!')
+            window.location.href = "cliente.html"
+        }
     })
 }
 
-const opcao = document.getElementById('opcoes')
+/*const opcao = document.getElementById('opcoes')
 opcao.innerHTML = ''
 
 const par = document.createElement('p')
@@ -73,10 +40,19 @@ btnAtualizar.addEventListener('submit', () => modificarCliente(cliente.id))
 const btnCriar = document.createElement('button')
 btnCriar.className = 'btn btn-primary mt-2 btn2'
 btnCriar.innerText = 'Cadastrar'
-btnCriar.addEventListener('submit', () => {
-    const form = document.getElementById('formCadastro')
-    criarCliente(form)})
-
+btnCriar.addEventListener('click', () => {
+    const formReg = document.getElementById('formCadastro')
+    console.log(formReg.target.nome.value)
+    //alert(form.target.nome)
+    //criarCliente(form)
+})
 opcao.appendChild(btnCriar)
 opcao.appendChild(par)
-opcao.appendChild(btnAtualizar)
+opcao.appendChild(btnAtualizar)*/
+
+const formReg = document.getElementById('formCadastro')
+formReg.addEventListener('submit', (event) => {
+    event.preventDefault()
+    //console.log(event.target.nome.value)
+    criarCliente(event)
+})
--- js/clienteList.js
+++ js/clienteList.js
@@ -35,7 +35,6 @@ function procurarCliente(id){
             atualizarClientes()
             throw erro("Cliente não existe")
         }
-        console.log(resposta.status)
         return resposta.json()
     })
     .catch(erro => alert('Cliente não existe!'))
@@ -65,8 +64,8 @@ function procurarCliente(id){
         //botao Update
         const btnUpdate = document.createElement('button')
         btnUpdate.className = "btn2"
-        btnUpdate.innerText = "Atualizar"
-        btnUpdate.addEventListener('click', () => modificarCliente(clienteDados.id))
+        btnUpdate.innerText = "Alterar"
+        btnUpdate.addEventListener('click', () => alterarCliente(clienteDados.id))
         ul.appendChild(btnUpdate)
 
         ul.appendChild(par)
@@ -88,11 +87,158 @@ function deletarCliente(id){
         if(resposta.status != 200){
             alert('Erro ao excluir!')
         }
+        else alert('Excluído com sucesso!')
         atualizarClientes()
     })
 }
 
-function modificarCliente(id){
+function alterarCliente(id){
+    const client = fetch(`http://localhost:3000/clientes/${id}`)
+    .then(resposta => {
+        if(resposta.status != 200){
+            atualizarClientes()
+            throw erro("Cliente não existe")
+        }
+        return resposta.json()
+    })
+    .catch(erro => alert('Cliente não existe!'))
+    .then(clienteDados => {
+
+        const ul = document.getElementById('listaClientes')
+        const par = document.createElement('p')
+        ul.innerHTML = ''
+
+        const attCliente = document.createElement('form')
+        attCliente.className = 'reg'
+        attCliente.method = 'post'
+        //attCliente.action = 'submit.php'
+        attCliente.id = 'formAtt'
+        
+        const attId = document.createElement('input')
+        attId.type = 'hidden'
+        attId.id = 'id'
+        attId.value = clienteDados.id
+
+        let attNome = document.createElement('input')
+        attNome.setAttribute("type", "text")
+        attNome.setAttribute('name', 'nome')
+        attNome.setAttribute('placeholder', 'Nome')
+        attNome.setAttribute('id', 'nome')
+        attNome.setAttribute('class', 'form-control')
+        attNome.setAttribute('value', clienteDados.nome)
+
+        let attCpf = document.createElement('input')
+        attCpf.setAttribute("type", "text")
+        attCpf.setAttribute('name', 'cpf')
+        attCpf.setAttribute('placeholder', 'CPF')
+        attCpf.setAttribute('id', 'cpf')
+        attCpf.setAttribute('class', 'form-control')
+        attCpf.setAttribute('value', clienteDados.cpf)
+
+        let attTelefone = document.createElement('input')
+        attTelefone.setAttribute("type", "text")
+        attTelefone.setAttribute('name', 'telefone')
+        attTelefone.setAttribute('placeholder', 'Telefone')
+        attTelefone.setAttribute('id', 'telefone')
+        attTelefone.setAttribute('class', 'form-control')
+        attTelefone.setAttribute('value', clienteDados.telefone)
+
+        let attEmail = document.createElement('input')
+        attEmail.setAttribute("type", "text")
+        attEmail.setAttribute('name', 'email')
+        attEmail.setAttribute('placeholder', 'E-mail')
+        attEmail.setAttribute('id', 'email')
+        attEmail.setAttribute('class', 'form-control')
+        attEmail.setAttribute('value', clienteDados.email)
+
+        let attCep = document.createElement('input')
+        attCep.setAttribute("type", "text")
+        attCep.setAttribute('name', 'cep')
+        attCep.setAttribute('placeholder', 'Cep')
+        attCep.setAttribute('id', 'cep')
+        attCep.setAttribute('class', 'form-control')
+        attCep.setAttribute('value', clienteDados.cep)
+
+        let attEstado = document.createElement('input')
+        attEstado.setAttribute("type", "text")
+        attEstado.setAttribute('name', 'estado')
+        attEstado.setAttribute('placeholder', 'Estado')
+        attEstado.setAttribute('id', 'estado')
+        attEstado.setAttribute('class', 'form-control')
+        attEstado.setAttribute('value', clienteDados.estado)
+
+        let attCidade = document.createElement('input')
+        attCidade.setAttribute("type", "text")
+        attCidade.setAttribute('name', 'cidade')
+        attCidade.setAttribute('placeholder', 'Cidade')
+        attCidade.setAttribute('id', 'cidade')
+        attCidade.setAttribute('class', 'form-control')
+        attCidade.setAttribute('value', clienteDados.cidade)
+
+        let attBairro = document.createElement('input')
+        attBairro.setAttribute("type", "text")
+        attBairro.setAttribute('name', 'bairro')
+        attBairro.setAttribute('placeholder', 'Bairro')
+        attBairro.setAttribute('id', 'bairro')
+        attBairro.setAttribute('class', 'form-control')
+        attBairro.setAttribute('value', clienteDados.bairro)
+
+        let attEndereco = document.createElement('input')
+        attEndereco.setAttribute("type", "text")
+        attEndereco.setAttribute('name', 'endereco')
+        attEndereco.setAttribute('placeholder', 'Endereço')
+        attEndereco.setAttribute('id', 'endereco')
+        attEndereco.setAttribute('class', 'form-control')
+        attEndereco.setAttribute('value', clienteDados.endereco)
+
+        let attComplemento = document.createElement('input')
+        attComplemento.setAttribute("type", "text")
+        attComplemento.setAttribute('name', 'complemento')
+        attComplemento.setAttribute('placeholder', 'Complemento')
+        attComplemento.setAttribute('id', 'complemento')
+        attComplemento.setAttribute('class', 'form-control')
+        attComplemento.setAttribute('value', clienteDados.complemento)
+
+        const submit = document.createElement('button')
+        submit.className = 'btn2'
+        submit.innerText = 'Alterar'
+        /*submit.addEventListener('click', (event) => {
+            event.preventDefault()
+            console.log(event.target.nome)
+            //modificarCliente(event)
+        })*/
+
+        attCliente.appendChild(attId)
+
+        attCliente.appendChild(attNome)
+        attCliente.appendChild(attCpf)
+        attCliente.appendChild(attTelefone)
+        attCliente.appendChild(attEmail)
+
+        attCliente.appendChild(par)
+
+        attCliente.appendChild(attCep)
+        attCliente.appendChild(attEstado)
+        attCliente.appendChild(attCidade)
+        attCliente.appendChild(attBairro)
+        attCliente.appendChild(attEndereco)
+        attCliente.appendChild(attComplemento)
+        attCliente.appendChild(submit)
+
+        attCliente.appendChild(par)
+
+        ul.appendChild(attCliente)
+
+        const Att = document.getElementById('formAtt')
+        Att.addEventListener('submit', (event) => {
+            event.preventDefault()
+            //console.log(event.target.id.value)
+            modificarCliente(event)
+        })
+    })
+}
+
+function modificarCliente(form){
     const clienteAtt = {
         nome: form.target.nome.value,
         cpf: form.target.cpf.value,
@@ -105,7 +251,7 @@ function modificarCliente(id){
         endereco: form.target.endereco.value,
         complemento: form.target.complemento.value
     }
-    fetch(`http://localhost:3000/clientes/${id}`,{
+    fetch(`http://localhost:3000/clientes/${form.target.id.value}`,{
         method: 'PUT',
         headers: {'Content-type':'application/json'},
         body: JSON.stringify(clienteAtt)
@@ -115,6 +261,6 @@ function modificarCliente(id){
             alert('Erro ao modificar!')
         }
         else alert('Modificado com sucesso!')
-        atualizarContatos()
+        window.location.href = "clienteList.html"
     })
 }
