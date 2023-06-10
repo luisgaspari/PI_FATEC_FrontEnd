afaef4f9d37f7efe4375980c378653d20cd87866
Caio Assis <caio.ab.assis@gmail.com>
2023-06-10 16:03:05 -0300

    Finaliza Produto

--- js/produto.js
+++ js/produto.js
@@ -1,12 +1,12 @@
-function atualizarClientes() {
+function atualizarProdutos() {
 
-    const ul = document.getElementById('listaClientes')
+    const ul = document.getElementById('listaProdutos')
     ul.innerHTML = ''
 
-    const clientes = fetch('https://pi-fatec2s-maracujadesign.onrender.com/clientes/')
+    const produtos = fetch('https://pi-fatec2s-maracujadesign.onrender.com/produtos/')
     .then(resposta => resposta.json())
-    .then(clientesDados => {
-        clientesDados.forEach(cliente => {
+    .then(produtosDados => {
+        produtosDados.forEach(produto => {
 
             const div = document.createElement('div')
             const li = document.createElement('input')
@@ -14,89 +14,79 @@ function atualizarClientes() {
             li.setAttribute("type", "submit");
             li.setAttribute('data-toggle', 'modal')
             li.setAttribute('data-target', '#listModal')
-            li.addEventListener('click', () => listarCliente(cliente.id))
-            li.value = `ID: ${cliente.id}
-Nome: ${cliente.nome}
-E-mail: ${cliente.email}
-Telefone: ${cliente.telefone}`
+            li.addEventListener('click', () => listarProduto(produto.id))
+            li.value = `ID: ${produto.id}
+Produto: ${produto.descricao}
+Tipo: ${produto.tipoProduto}
+Valor: R$${produto.valorVenda}`
 
             div.appendChild(li)
 
-            // Adiciona um botão de excluir para cada Cliente
+            // Adiciona um botão de excluir para cada produto
             const botaoExcluir = document.createElement("button")
             botaoExcluir.textContent = "Excluir"
             botaoExcluir.className = "btn btn-danger btn-sm btn2"
-            botaoExcluir.addEventListener('click', () => deletarCliente(cliente.id))
+            botaoExcluir.addEventListener('click', () => deletarProduto(produto.id))
             div.appendChild(botaoExcluir)
 
-            // Adiciona um botão de atualizar para cada Cliente
+            // Adiciona um botão de atualizar para cada produto
             const botaoAtualizar = document.createElement("button")
             botaoAtualizar.textContent = "Atualizar"
             botaoAtualizar.className = "btn btn-warning btn-sm btn2"
             botaoAtualizar.setAttribute('data-toggle', 'modal')
             botaoAtualizar.setAttribute('data-target', '#updateModal')
-            botaoAtualizar.addEventListener("click", () => procurarCliente(cliente.id))
+            botaoAtualizar.addEventListener("click", () => procurarProduto(produto.id))
             div.appendChild(botaoAtualizar)
 
-            document.getElementById("listaClientes").appendChild(div)
+            document.getElementById("listaProdutos").appendChild(div)
         }) 
     })
 }
 
-function procurarCliente(id){
-    const client = fetch(`https://pi-fatec2s-maracujadesign.onrender.com/clientes/${id}`)
+function procurarProduto(id){
+    const product = fetch(`https://pi-fatec2s-maracujadesign.onrender.com/produtos/${id}`)
     .then(resposta => {
         if(resposta.status != 200){
-            atualizarClientes()
-            throw erro("Cliente não existe")
+            atualizarProdutos()
+            throw erro("Produto não existe")
         }
         return resposta.json()
     })
-    .catch(erro => alert('Cliente não existe!'))
-    .then(clienteDados => {
-
-        document.getElementById('attId').value = clienteDados.id
-        document.getElementById('attNome').value = clienteDados.nome
-        document.getElementById('attTelefone').value = clienteDados.telefone
-        document.getElementById('attEmail').value = clienteDados.email
-        document.getElementById('attCpf').value = clienteDados.cpf
-        document.getElementById('attCep').value = clienteDados.cep
-        document.getElementById('attEstado').value = clienteDados.estado
-        document.getElementById('attCidade').value = clienteDados.cidade
-        document.getElementById('attBairro').value = clienteDados.bairro
-        document.getElementById('attEndereco').value = clienteDados.endereco
-        document.getElementById('attComplemento').value = clienteDados.complemento
+    .catch(erro => alert('Produto não existe!'))
+    .then(produtoDados => {
+        document.getElementById('attId').value = produtoDados.id
+        document.getElementById('attDescricao').value = produtoDados.descricao
+        document.getElementById('attFichaTecnica').value = produtoDados.fichaTecnica
+        document.getElementById('attUnidadeMedida').value = produtoDados.unidadeMedida
+        document.getElementById('attTipoProduto').value = produtoDados.tipoProduto
+        document.getElementById('attValorCompra').value = produtoDados.valorCompra
+        document.getElementById('attValorVenda').value = produtoDados.valorVenda
     })
 }
 
-function listarCliente(id){
-    const client = fetch(`https://pi-fatec2s-maracujadesign.onrender.com/clientes/${id}`)
+function listarProduto(id){
+    const product = fetch(`https://pi-fatec2s-maracujadesign.onrender.com/produtos/${id}`)
     .then(resposta => {
         if(resposta.status != 200){
-            atualizarClientes()
-            throw erro("Cliente não existe")
+            atualizarProdutos()
+            throw erro("Produto não existe")
         }
         return resposta.json()
     })
-    .catch(erro => alert('Cliente não existe!'))
-    .then(clienteDados => {
-
-        document.getElementById('listId').value = clienteDados.id
-        document.getElementById('listNome').value = clienteDados.nome
-        document.getElementById('listTelefone').value = clienteDados.telefone
-        document.getElementById('listEmail').value = clienteDados.email
-        document.getElementById('listCpf').value = clienteDados.cpf
-        document.getElementById('listCep').value = clienteDados.cep
-        document.getElementById('listEstado').value = clienteDados.estado
-        document.getElementById('listCidade').value = clienteDados.cidade
-        document.getElementById('listBairro').value = clienteDados.bairro
-        document.getElementById('listEndereco').value = clienteDados.endereco
-        document.getElementById('listComplemento').value = clienteDados.complemento
+    .catch(erro => alert('Produto não existe!'))
+    .then(produtoDados => {
+        document.getElementById('listId').value = produtoDados.id
+        document.getElementById('listDescricao').value = produtoDados.descricao
+        document.getElementById('listFichaTecnica').value = produtoDados.fichaTecnica
+        document.getElementById('listUnidadeMedida').value = produtoDados.unidadeMedida
+        document.getElementById('listTipoProduto').value = produtoDados.tipoProduto
+        document.getElementById('listValorCompra').value = produtoDados.valorCompra
+        document.getElementById('listValorVenda').value = produtoDados.valorVenda
     })
 }
 
-function deletarCliente(id){
-    fetch(`https://pi-fatec2s-maracujadesign.onrender.com/clientes/${id}`,{
+function deletarProduto(id){
+    fetch(`https://pi-fatec2s-maracujadesign.onrender.com/produtos/${id}`,{
         method: 'DELETE'
     })
     .then(resposta => {
@@ -104,56 +94,48 @@ function deletarCliente(id){
             alert('Erro ao excluir!')
         }
         else alert('Excluído com sucesso!')
-        atualizarClientes()
+        atualizarProdutos()
     })
 }
 
-function modificarCliente(form){
-    const clienteAtt = {
-        nome: form.target.attNome.value,
-        cpf: form.target.attCpf.value,
-        telefone: form.target.attTelefone.value,
-        email: form.target.attEmail.value,
-        cep: form.target.attCep.value,
-        estado: form.target.attEstado.value,
-        cidade: form.target.attCidade.value,
-        bairro: form.target.attBairro.value,
-        endereco: form.target.attEndereco.value,
-        complemento: form.target.attComplemento.value
+function modificarProduto(form){
+    const produtoAtt = {
+        descricao: form.target.attDescricao.value,
+        fichaTecnica: form.target.attFichaTecnica.value,
+        unidadeMedida: form.target.attUnidadeMedida.value,
+        tipoProduto: form.target.attTipoProduto.value,
+        valorCompra: form.target.attValorCompra.value,
+        valorVenda: form.target.attValorVenda.value
     }
-    fetch(`https://pi-fatec2s-maracujadesign.onrender.com/clientes/${form.target.attId.value}`,{
+    fetch(`https://pi-fatec2s-maracujadesign.onrender.com/produtos/${form.target.attId.value}`,{
         method: 'PUT',
         headers: {'Content-type':'application/json'},
-        body: JSON.stringify(clienteAtt)
+        body: JSON.stringify(produtoAtt)
     })
     .then(resposta => {
         if(resposta.status != 200){
             alert('Erro ao modificar!')
         }
         else alert('Modificado com sucesso!')
-        window.location.href = "cliente.html"
+        window.location.href = "produto.html"
     })
 }
 
 
 
-function criarCliente(form){
-    const clienteNovo = {
-        nome: form.target.nome.value,
-        cpf: form.target.cpf.value,
-        telefone: form.target.telefone.value,
-        email: form.target.email.value,
-        cep: form.target.cep.value,
-        estado: form.target.estado.value,
-        cidade: form.target.cidade.value,
-        bairro: form.target.bairro.value,
-        endereco: form.target.endereco.value,
-        complemento: form.target.complemento.value
+function criarProduto(form){
+    const produtoNovo = {
+        descricao: form.target.descricao.value,
+        fichaTecnica: form.target.fichaTecnica.value,
+        unidadeMedida: form.target.unidadeMedida.value,
+        tipoProduto: form.target.tipoProduto.value,
+        valorCompra: form.target.valorCompra.value,
+        valorVenda: form.target.valorVenda.value
     }
-    fetch(`https://pi-fatec2s-maracujadesign.onrender.com/clientes`,{
+    fetch(`https://pi-fatec2s-maracujadesign.onrender.com/produtos`,{
         method: 'POST',
         headers: {'Content-type':'application/json'},
-        body: JSON.stringify(clienteNovo)
+        body: JSON.stringify(produtoNovo)
     })
     .then(resposta => {
         if(resposta.status != 201){
@@ -161,7 +143,7 @@ function criarCliente(form){
         }
         else {
             alert('Cadastrado com sucesso!')
-            window.location.href = "cliente.html"
+            window.location.href = "produto.html"
         }
     })
 }
@@ -170,14 +152,14 @@ const formReg = document.getElementById('formCadastro')
 formReg.addEventListener('submit', (event) => {
     event.preventDefault()
     //console.log(event.target.nome.value)
-    criarCliente(event)
+    criarProduto(event)
 })
 
 
-atualizarClientes()
+atualizarProdutos()
 const Att = document.getElementById('formAtt')
 Att.addEventListener('submit', (event) => {
     event.preventDefault()
     //console.log(event.target.attNome.value)
-    modificarCliente(event)
+    modificarProduto(event)
 })
