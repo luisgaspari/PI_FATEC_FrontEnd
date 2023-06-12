b4ade42d6215d82766d51df47961bcf0ea21ce25
LucasFelipeJesus <lucasfelipejesus@gmail.com>
2023-06-12 11:22:56 -0300

    Ajuste Compras

--- js/compras.js
+++ js/compras.js
@@ -1,48 +1,48 @@
-listaFornecedores()
-
-function mostrarCompras() {
+const host = "https://pi-fatec2s-maracujadesign.onrender.com"
+// Lista Todos os Pedidos de Compras
+function listarCompras() {
     document.getElementById("listaCompras").innerHTML = ""
 
-    const pCompras = fetch(
-        "https://pi-fatec2s-maracujadesign.onrender.com/compras"
-    )
+    const compras = fetch(`${host}/compras`)
         .then((resposta) => resposta.json())
-        .then((pCompras) => {
-            pCompras.forEach((compra) => {
+        .then((compras) => {
+            compras.forEach((compra) => {
                 data = new Date(compra.dataPedido)
+                data2 = new Date(compra.dataEntrega)
                 formatData = data.toLocaleDateString("pt-BR", {
                     timeZone: "UTC",
                 })
-                data2 = new Date(compra.dataEntrega)
                 formatData2 = data2.toLocaleDateString("pt-BR", {
                     timeZone: "UTC",
                 })
-                formatTotal = compra.totalPedido.toFixed(2)
+                formatTotal = compra.totalCompra.toFixed(2)
                 const li = document.createElement("li")
                 li.className =
                     "list-group-item d-flex justify-content-between list-group-item-secondary"
-                li.innerText = `ID: ${compra.id} - Data do Pedido: ${formatData} - Data de Entrega: ${formatData2} - Forma Pagto.: ${compra.formaPagamento} - Valor Total: ${formatTotal}
-                Cliente: ${compra.fornecedor.nome} - Telefone: ${compra.fornecedor.telefone} - Email: ${compra.fornecedor.email}`
+                li.innerText = `ID: ${compra.id} - Data do compra: ${formatData} - Data da Entrega: ${formatData2} - Forma Pagto.: ${compra.formaPagamento} - Valor Total: ${formatTotal}
+                fornecedor: ${compra.fornecedor.nome} - Telefone: ${compra.fornecedor.telefone} - Email: ${compra.fornecedor.email}`
 
                 const span = document.createElement("span")
 
-                // Adiciona um botão de excluir para cada Compra
                 const botaoExcluir = document.createElement("button")
                 botaoExcluir.textContent = "Excluir"
                 botaoExcluir.className = "btn btn-danger btn-sm"
                 botaoExcluir.addEventListener("click", () =>
-                    deleteCompra(compra.id)
+                    excluirCompra(compra.id)
                 )
                 span.appendChild(botaoExcluir)
 
-                // Adiciona um botão de atualizar para cada compra
+                const space = document.createElement("span")
+                space.textContent = " "
+                span.appendChild(space)
+
                 const botaoAtualizar = document.createElement("button")
                 botaoAtualizar.textContent = "Atualizar"
                 botaoAtualizar.className = "btn btn-warning btn-sm"
                 botaoAtualizar.setAttribute("data-toggle", "modal")
                 botaoAtualizar.setAttribute("data-target", "#updateModal")
                 botaoAtualizar.addEventListener("click", () =>
-                    mostrarCompras(compra)
+                    mostrarCompra(compra)
                 )
                 span.appendChild(botaoAtualizar)
 
@@ -53,22 +53,23 @@ function mostrarCompras() {
         })
 }
 
-mostrarCompras()
+listarCompras()
 
-// Mostrar Dados do Pedido de Compra
-function deleteCompra(id) {
-    fetch(`https://pi-fatec2s-maracujadesign.onrender.com/compras/${id}`, {
+// Exclui o Pedido de Compra
+function excluirCompra(id) {
+    fetch(`${host}/compras/${id}`, {
         method: "DELETE",
     }).then((resposta) => {
         if (resposta.status != 200) {
-            alert("Erro ao excluir o Pedido de Compra!")
+            alert("Erro ao excluir o Pedido de Compras!")
         }
-        alert("Pedido de Compra excluído com sucesso!")
-        atualizarCompra()
+        alert("Pedido de Compras excluído com sucesso!")
+        listarCompras()
     })
 }
-// Mostrar Dados do Pedido de Compra
-function mostrarCompras(compra) {
+
+// Mostrar Dados do Pedido de Compras
+function mostrarCompra(compra) {
     document.getElementById("updateID").value = compra.id
     document.getElementById("updateFornecedor").value = compra.fornecedorId
     document.getElementById("updateTelefone").value = compra.fornecedor.telefone
@@ -84,7 +85,7 @@ function mostrarCompras(compra) {
     document.getElementById("updateDataPedido").value = formatDate(
         compra.dataPedido
     )
-    document.getElementById("updateDataEntrega").value = formatDate2(
+    document.getElementById("updateDataEntrega").value = formatDate(
         compra.dataEntrega
     )
     document.getElementById("updateValorTotal").value =
@@ -92,24 +93,24 @@ function mostrarCompras(compra) {
     mostrarProdutos(compra.id)
 }
 
-// Lista todos os  Produtos do Pedido de compras
+// Lista todos os  Produtos do Pedido
 function mostrarProdutos(id) {
     document.getElementById("listaProdutosCadastrar").innerHTML = ""
     document.getElementById("listaProdutosAtualizar").innerHTML = ""
 
-    const compras = fetch(`${host}/vendas/${id}`)
+    const compras = fetch(`${host}/compras/${id}`)
         .then((resposta) => resposta.json())
         .then((compras) => {
-            compras.produtos.forEach((compra) => {
+            compras.produtos.forEach((produto) => {
                 console.log(produto)
-                var formatValorCompra = produto.valorCompra.toFixed(2)
+                var formatvalorCompra = produto.valorCompra.toFixed(2)
                 var formatValorProduto =
                     produto.detCompra.valorProduto.toFixed(2)
                 const li = document.createElement("li")
                 li.className = "list-group-item d-flex justify-content-between"
                 li.innerText = `ID: ${produto.id} - Descrição: ${produto.descricao} ${produto.fichaTecnica}
                 Personalização: ${produto.detCompra.personalizacao}
-                Quantidade: ${produto.detCompra.quantidade} - ${produto.unidadeMedida} - Preço Unit.: ${formatValorCompra} - Total: ${formatValorProduto}`
+                Quantidade: ${produto.detCompra.quantidade} - ${produto.unidadeMedida} - Preço Unit.: ${formatvalorCompra} - Total: ${formatValorProduto}`
 
                 const span = document.createElement("span")
 
@@ -155,37 +156,35 @@ document
         event.preventDefault()
         adicionarProduto(event)
     })
+
 // Adiciona Produto ao Pedido no Banco de Dados
 function adicionarProduto(form) {
-    const pedidoId = form.target.prodPedID.value
+    const compraId = form.target.prodPedID.value
     const produto = {
         produtoId: form.target.selectProduto.value,
         personalizacao: form.target.personalizacao.value,
         quantidade: form.target.quantidade.value,
     }
-    fetch(
-        `https://pi-fatec2s-maracujadesign.onrender.com/vendas/${pedidoId}/produtos/`,
-        {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(produto),
-        }
-    ).then((resposta) => {
+    fetch(`${host}/compras/${compraId}/produtos`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(produto),
+    }).then((resposta) => {
         if (resposta.status != 200 && resposta.status != 201) {
-            alert("Erro ao adicionar o produto no Pedido de Compra!")
+            alert("Erro ao adicionar o produto no Pedido de Compras!")
         } else {
             alert("Produto adicionado com sucesso!")
         }
     })
-    mostrarProdutos(pedidoId)
+    mostrarProdutos(compraId)
 }
 
-// Exclui Produto do Pedido de compras no Banco de Dados
-function excluirProduto(pedidoId, produtoId) {
+// Exclui Produto do Pedido no Banco de Dados
+function excluirProduto(compraId, produtoId) {
     const produto = {
         produtoId: produtoId,
     }
-    fetch(`${host}/vendas/${pedidoId}/produtos/`, {
+    fetch(`${host}/compras/${compraId}/produtos/`, {
         method: "DELETE",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(produto),
@@ -195,7 +194,7 @@ function excluirProduto(pedidoId, produtoId) {
         }
         alert("Produto excluído com sucesso!")
     })
-    mostrarProdutos(pedidoId)
+    mostrarProdutos(compraId)
 }
 
 // Aguarda o click no botão Atualizar
@@ -203,37 +202,40 @@ document
     .getElementById("formAtualizar")
     .addEventListener("submit", function (event) {
         event.preventDefault()
-        atualizarPedido(event)
+        atualizarCompra(event)
     })
+
 // Atualiza Pedido no Banco de Dados
-function modificarCompra(form) {
-    const compraAtt = {
-        fornecedorId: form.target.fornecedorId.value,
-        dataPedido: form.target.dataPedido.value,
-        dataEntrega: form.target.dataEntrega.value,
-        formaPagamento: form.target.formaPagamento.value,
-        totalPedido: form.target.totalPedido.value,
+function atualizarCompra(form) {
+    const compra = {
+        fornecedorId: form.target.updatefornecedor.value,
+        dataPedido: form.target.updateDataPedido.value,
+        dataEntrega: form.target.updateDataEntrega.value,
+        formaPagamento: form.target.updateFormaPagamento.value,
     }
-    fetch(
-        `https://pi-fatec2s-maracujadesign.onrender.com/compras/${form.target.id.value}`,
-        {
-            method: "PUT",
-            headers: { "Content-type": "application/json" },
-            body: JSON.stringify(compraAtt),
+
+    fetch(`${host}/compras/${form.target.updateID.value}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(compra),
+    }).then((resposta) => {
+        if (resposta.status != 200 && resposta.status != 201) {
+            alert("Erro ao atualizar o Pedido de Compra!")
+        } else {
+            alert("Pedido de Compra atualizado com sucesso!")
         }
-    ).then((resposta) => {
-        if (resposta.status != 200) {
-            alert("Erro ao Atualizar pedido de Compras!")
-        } else alert("Atualizado pedido de Compras com sucesso!")
-        window.location.href = "compras.html"
+        //form.target.reset()
+        listarCompras()
     })
 }
+
 // Abre o Formulário de Novo Pedido
 function onOpenFormCadastrar() {
     document.getElementById("btnAddProdutoCadastrar").disabled = true
     document.getElementById("novoModalLabel").innerHTML = "Novo Pedido"
     $("#novoModal").modal("show")
 }
+
 // Aguarda o click no botão Salvar
 document
     .getElementById("formCadastrar")
@@ -241,17 +243,18 @@ document
         event.preventDefault()
         cadastrarCompra(event)
     })
-// Cadastra Novo Pedido de compras no Banco de Dados
+
+// Cadastra Novo Pedido no Banco de Dados
 function cadastrarCompra(form) {
     const compra = {
-        fornecedorId: form.target.selectFornecedor.value,
+        fornecedorId: form.target.selectfornecedor.value,
         dataPedido: form.target.dataPedido.value,
         dataEntrega: form.target.dataEntrega.value,
         formaPagamento: form.target.formaPagamento.value,
         totalPedido: 0,
     }
 
-    fetch("https://pi-fatec2s-maracujadesign.onrender.com/compras", {
+    fetch(`${host}/compras`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(compra),
@@ -264,14 +267,14 @@ function cadastrarCompra(form) {
                 document.getElementById(
                     "novoModalLabel"
                 ).innerHTML = `Pedido #${compraId.id}`
-                document.getElementById("pedidoID").value = compraId.id
+                document.getElementById("compraID").value = compraId.id
                 document.getElementById(
                     "btnAddProdutoCadastrar"
                 ).disabled = false
                 alert("Pedido de Compra cadastrado com sucesso!")
             }
         })
-    mostrarCompras()
+    listarCompras()
 }
 
 // Função para formatar Data
@@ -286,12 +289,12 @@ function formatDate(date) {
 
     return [year, month, day].join("-")
 }
+
 listaFornecedores()
+
 // Lista Fornecedores no campo Select do Formulário Cadastrar
 function listaFornecedores() {
-    const fornecedores = fetch(
-        "https://pi-fatec2s-maracujadesign.onrender.com/fornecedores"
-    )
+    const fornecedores = fetch(`${host}/Fornecedores`)
         .then((resposta) => resposta.json())
         .then((fornecedores) => {
             var select = document.getElementById("selectFornecedor")
@@ -308,9 +311,7 @@ listaUpdateFornecedores()
 
 // Lista Fornecedores no campo Select do Formulário Atualizar
 function listaUpdateFornecedores() {
-    const fornecedores = fetch(
-        `https://pi-fatec2s-maracujadesign.onrender.com/fornecedores`
-    )
+    const fornecedores = fetch(`${host}/Fornecedores`)
         .then((resposta) => resposta.json())
         .then((fornecedores) => {
             var select = document.getElementById("updateFornecedor")
@@ -322,28 +323,10 @@ function listaUpdateFornecedores() {
             })
         })
 }
-// Mostra dados do Fornecedor no Formulário Atualizar
-function updateDadosFornecedor(id) {
-    const fornecedor = fetch(
-        `https://pi-fatec2s-maracujadesign.onrender.com/fornecedores/${id}`
-    )
-        .then((resposta) => resposta.json())
-        .then((fornecedor) => {
-            document.getElementById("showTelefone").value = fornecedor.telefone
-            document.getElementById("showEmail").value = fornecedor.email
-            document.getElementById("showEndereco").value = fornecedor.endereco
-            document.getElementById("showComplemento").value =
-                fornecedor.complemento
-            document.getElementById("showCidade").value = fornecedor.cidade
-            document.getElementById("showEstado").value = fornecedor.estado
-            document.getElementById("showCEP").value = fornecedor.cep
-        })
-}
+
 // Mostra dados do Fornecedor no Formulário Cadastrar
-function mostraDadosFornecedores(id) {
-    const fornecedor = fetch(
-        `https://pi-fatec2s-maracujadesign.onrender.com/fornecedores/${id}`
-    )
+function mostraDadosFornecedor(id) {
+    const fornecedor = fetch(`${host}/Fornecedores/${id}`)
         .then((resposta) => resposta.json())
         .then((fornecedor) => {
             document.getElementById("showTelefone").value = fornecedor.telefone
@@ -354,33 +337,12 @@ function mostraDadosFornecedores(id) {
             document.getElementById("showCidade").value = fornecedor.cidade
             document.getElementById("showEstado").value = fornecedor.estado
             document.getElementById("showCEP").value = fornecedor.cep
-        })
-}
-// Mostra dados do Fornecedores no Formulário Atualizar
-function updateDadosFornecedor(id) {
-    const fornecedor = fetch(
-        `https://pi-fatec2s-maracujadesign.onrender.com/fornecedores/${id}`
-    )
-        .then((resposta) => resposta.json())
-        .then((fornecedor) => {
-            document.getElementById("updateTelefone").value =
-                fornecedor.telefone
-            document.getElementById("updateEmail").value = fornecedor.email
-            document.getElementById("updateEndereco").value =
-                fornecedor.endereco
-            document.getElementById("updateComplemento").value =
-                fornecedor.complemento
-            document.getElementById("updateCidade").value = fornecedor.cidade
-            document.getElementById("updateEstado").value = fornecedor.estado
-            document.getElementById("updateCEP").value = fornecedor.cep
         })
 }
 
-// Mostra dados do Fornecedor no Formulário Atualizar
-function updateDadosCliente(id) {
-    const fornecedor = fetch(
-        `https://pi-fatec2s-maracujadesign.onrender.com/fornecedores/${id}`
-    )
+// Mostra dados dos fornecedores no Formulário Atualizar
+function updateDadosFornecedor(id) {
+    const fornecedor = fetch(`${host}/Fornecedores/${id}`)
         .then((resposta) => resposta.json())
         .then((fornecedor) => {
             document.getElementById("updateTelefone").value =
@@ -398,10 +360,9 @@ function updateDadosCliente(id) {
 
 listaProdutos()
 
+// Lista Produtos no campo Select do Formulário Produtos
 function listaProdutos() {
-    const fornecedor = fetch(
-        "https://pi-fatec2s-maracujadesign.onrender.com/produtos"
-    )
+    const fornecedores = fetch(`${host}/produtos`)
         .then((resposta) => resposta.json())
         .then((produtos) => {
             var select = document.getElementById("selectProduto")
@@ -435,7 +396,7 @@ function mostraDadosProduto(id) {
         })
 }
 
-// Calcula o Valor total do Pedido de Venda
+// Calcula o Valor total do Pedido de Compra
 function calculaTotalProduto() {
     var valorCompra = document.getElementById("valorCompra").value
     if (valorCompra > 0) {
